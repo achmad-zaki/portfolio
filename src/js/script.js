69fa926a5ff86bfd0bb9e1178628203bd684a238
ahmad zaki <achmadzaki10d@gmail.com>
2023-09-26 15:41:18 +0700

    home section

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,21 @@
 const handleToggleSidebar = () => {
     const sidebar = document.getElementById("sidebar");
-    if (sidebar.classList.contains("right-0")) {
-        sidebar.classList.remove("right-0");
-        sidebar.classList.add("-right-full");
-    } else {
+    if (sidebar.classList.contains("-right-full")) {
         sidebar.classList.remove("-right-full");
         sidebar.classList.add("right-0");
+    } else {
+        sidebar.classList.remove("right-0");
+        sidebar.classList.add("-right-full");
     }
 }
+
+const navLinks = document.querySelectorAll("#sidebar li a")
+navLinks.forEach(link => {
+    link.addEventListener("click" , function() {
+        navLinks.forEach(link => {
+            link.classList.remove("text-primary");
+        });
+
+        this.classList.add("text-primary");
+    });
+});
